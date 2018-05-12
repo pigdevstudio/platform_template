13720bf6c484483c70b0424a3f4deec14eb944f0
Henrique Campos <henriiquecampos@gmail.com>
2018-05-12 18:40:35 -0300

    Revert "Fixed #5"
    This reverts commit 12030e968e5fa2f88f1032189c68812b2264c206.

--- actors/kinematic_character.gd
+++ actors/kinematic_character.gd
@@ -1,10 +1,9 @@
 extends KinematicBody2D
 
-enum states {IDLE, WALK, JUMP, FALL}
-
 export (int) var walk_speed = 100
 export (int) var jump_height = 800
 export (int) var max_jumps = 2
+export (int) var dash_length
 
 var direction = 1
 var velocity = Vector2(0, 0)
@@ -18,24 +17,40 @@ const FLOOR_NORMAL = Vector2(0, -1)
 const SLOPE_STOP_SPEED = 200
 
 func set_state(new_state):
-	state_machine.set_state(new_state)
+	state_machine.push_state(new_state)
+	state_machine.resolve_states()
+
+func dash():
+	set_state("dash")
+	var speed = (walk_speed * direction) * 3
+	velocity.x = speed
 
 func jump():
 	if jumps > 0:
-		return(-jump_height)
-	else:
-		return(velocity.y)
-	
+		set_state("jump")
+		velocity.y = -jump_height
+		jumps -= 1
+
+func fall():
+	set_state("fall")
+
 func cancel_jump():
 	velocity.y = 0
-	
+	fall()
+
 func walk():
+	set_state("walk")
 	var speed = walk_speed * direction
-	return(speed)
-	
+	velocity.x = speed
+
 func stop():
-	return(Vector2(0, velocity.y))
-	
+	velocity.x = 0
+
+func _ready():
+	set_state("idle")
+
 func _physics_process(delta):
 	velocity.y += GRAVITY
 	velocity = move_and_slide(velocity, FLOOR_NORMAL, SLOPE_STOP_SPEED, 4, deg2rad(46))
+	if is_on_floor():
+		jumps = max_jumps
