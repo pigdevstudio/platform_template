a5c8c2ffe51724667fe79a7e9e164a4f7ad10c59
Henrique Campos <henriiquecampos@gmail.com>
2018-05-17 10:20:03 -0300

    Added exception for jump setup
    This makes sure that the jump can setup in_jump_speed in its setup method

--- modules/state_machine/state_machine.gd
+++ modules/state_machine/state_machine.gd
@@ -8,7 +8,14 @@ func set_state(new_state):
 	if state != null:
 		state.clear()
 		emit_signal("state_changed", state, new_state)
-	new_state.setup(get_parent())
+		
+	if new_state.name == "jump":
+		if state.name == "dash":
+			new_state.setup(get_parent(), get_parent().dash_speed)
+		else:
+			new_state.setup(get_parent(), get_parent().walk_speed)
+	else:
+		new_state.setup(get_parent())
 	state = new_state
 	
 func get_state():
