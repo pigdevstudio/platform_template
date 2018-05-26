93c40bafbafe4a83dafa59dea9f7cd94aa692615
Henrique Campos <henriiquecampos@gmail.com>
2018-05-26 10:13:06 -0300

    Removed residual files

--- actors/boss.gd
+++
@@ -1,3 +0,0 @@
-extends "res://addons/platform_actors/platform_actor.gd"
-
-const MAX_WALL_JUMPS = 3
--- actors/player_kinematic_character.gd
+++
@@ -1,6 +0,0 @@
-extends "kinematic_character.gd"
-
-func handle_input():
-	pass
-func _input(event):
-	state_machine.state.handle_input(self, event)
