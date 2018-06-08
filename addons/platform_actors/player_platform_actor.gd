0c1145c6b5a1fabbf3eb6b45302d6c1e02b8c8c9
Henrique Campos <henriiquecampos@gmail.com>
2018-06-08 08:33:41 -0300

    Updated default actions for player one

--- addons/platform_actors/player_platform_actor.gd
+++ addons/platform_actors/player_platform_actor.gd
@@ -1,11 +1,11 @@
 extends "platform_actor.gd"
 
-export (String) var left = "left"
-export (String) var right = "right"
-export (String) var up = "up"
-export (String) var down = "down"
-export (String) var jump = "jump"
-export (String) var dash = "dash"
+export (String) var left = "left_1"
+export (String) var right = "right_1"
+export (String) var up = "up_1"
+export (String) var down = "down_1"
+export (String) var jump = "jump_1"
+export (String) var dash = "dash_1"
 func handle_input():
 	pass
 func _input(event):
