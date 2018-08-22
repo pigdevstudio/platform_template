d839baacc18b8d98fa7a4e81c45a3d1442aa8761
Henrique Campos <henriquecampos@pigdev.studio>
2018-08-22 08:57:38 -0300

    Fix #25

--- source/actors/state_machine/states/wall.gd
+++ source/actors/state_machine/states/wall.gd
@@ -12,7 +12,7 @@ func setup(actor, previous_state):
 	actor.emit_signal("perform_action", "wall")
 
 func input_process(actor, event):
-	if event.is_action_pressed(actor.jump):
+	if event.is_action_pressed(actor.jump) and actor.is_on_wall():
 		if Input.is_action_pressed(actor.dash):
 			actor.velocity.x = (wall_jump * normal.x)
 			actor.velocity.y = -wall_jump * dash_jump_multiplier
