e8175b896c7c415bfb8231e11ae1bb093e23489f
Henrique Campos <henriquecampos@pigdev.studio>
2018-09-14 14:53:10 -0300

    Fix some issues related to climb and ladders

--- source/actors/template/ladder_check.gd
+++ source/actors/template/ladder_check.gd
@@ -4,33 +4,31 @@ export (bool) var snap = false
 
 onready var actor = get_parent()
 var is_on_ladder = false
+var already_snap = false
+var center = null
 
 func _input(event):
+	if not is_on_ladder:
+		return
 	if event.is_action_pressed(actor.down) or event.is_action_pressed(actor.up):
-		if not is_on_ladder:
-			return
 		actor.set_collision_mask_bit(2, false)
 		actor.climb()
-		if not snap:
+		if not snap or already_snap:
 			return
 		#Searches for an area to snap to
-		var area = null
-		for a in get_overlapping_areas():
-			if not a.get_collision_layer_bit(2):
-				continue
-			area = a
-		actor.global_position.x = area.global_position.x
+		actor.global_position.x = center
+		already_snap = true
 
 func _on_ladder_check_body_entered(body):
-	if body.get_collision_layer_bit(2):
-		is_on_ladder = true
+	is_on_ladder = true
+	center = body.global_position.x
 
 func _on_ladder_check_area_entered(area):
-	if area.get_collision_layer_bit(2):
-		is_on_ladder = true
+	is_on_ladder = true
+	center = area.global_position.x
 
 func _on_ladder_check_area_exited(area):
-	if area.get_collision_layer_bit(2):
-		is_on_ladder = false
-		actor.set_collision_mask_bit(2, true)
-		actor.fall()
+	is_on_ladder = false
+	already_snap = false
+	actor.set_collision_mask_bit(2, true)
+	actor.fall()
