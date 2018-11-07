5ffd2d4165e02eb9feed63101fd9cce9b7c845a1
Henrique Campos <henriquecampos@pigdev.studio>
2018-11-07 19:10:14 -0200

    Change shake_camera to use offset instead of position
    Also change camera_rig to just layer shake_camera logic

--- source/juiceness/shake_camera/rigged_camera/camera_rig.gd
+++ source/juiceness/shake_camera/rigged_camera/camera_rig.gd
@@ -1,37 +1,10 @@
 extends RemoteTransform2D
 
-export (float) var shake_strength = 10
-
+var camera = null
 
 func _ready():
-	set_process(false)
+	camera = get_node(remote_path)
 
-func _process(delta):
-	position.x = rand_range(-shake_strength, shake_strength)
-	position.y = rand_range(-shake_strength, shake_strength)
-
-func shake(strength = shake_strength, time = $timer.wait_time):
-	
-	var initial_pos = position
-	var camera = get_node(remote_path)
-	var drag_h = camera.drag_margin_h_enabled
-	var drag_v = camera.drag_margin_v_enabled
-	camera.drag_margin_h_enabled = false
-	camera.drag_margin_v_enabled = false
-	var default_time = $timer.wait_time
-	var default_strength = shake_strength
-
-	shake_strength = strength
-	$timer.wait_time = time
-
-	set_process(true)
-	$timer.start()
-	yield($timer, "timeout")
-
-	set_process(false)
-	position = initial_pos
-	camera.drag_margin_h_enabled = drag_h
-	camera.drag_margin_v_enabled = drag_v
-	shake_strength = default_strength
-	$timer.wait_time = default_time
-	
+func shake(strength = camera.shake_strength, 
+	time = camera.get_node("timer").wait_time):
+	camera.shake(strength, time)
--- source/juiceness/shake_camera/shake_camera.gd
+++ source/juiceness/shake_camera/shake_camera.gd
@@ -7,16 +7,11 @@ func _ready():
 	set_process(false)
 
 func _process(delta):
-	position.x = rand_range(-shake_strength, shake_strength)
-	position.y = rand_range(-shake_strength, shake_strength)
+	offset.x = rand_range(-shake_strength, shake_strength)
+	offset.y = rand_range(-shake_strength, shake_strength)
 
 func shake(strength = shake_strength, time = $timer.wait_time):
 	
-	var initial_pos = position
-	var drag_h = drag_margin_h_enabled
-	var drag_v = drag_margin_v_enabled
-	drag_margin_h_enabled = false
-	drag_margin_v_enabled = false
 	var default_time = $timer.wait_time
 	var default_strength = shake_strength
 
@@ -28,9 +23,6 @@ func shake(strength = shake_strength, time = $timer.wait_time):
 	yield($timer, "timeout")
 
 	set_process(false)
-	position = initial_pos
-	drag_margin_h_enabled = drag_h
-	drag_margin_v_enabled = drag_v
+	offset = Vector2(0, 0)
 	shake_strength = default_strength
 	$timer.wait_time = default_time
-	
