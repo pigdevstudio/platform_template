fa6d23291c657a134708058f4f4e399d716d289b
Henrique Campos <henriquecampos@pigdev.studio>
2019-11-12 09:21:18 -0300

    Fix wrong export hint on pop_label.gd

--- source/juiceness/pop_score/pop_label.gd
+++ source/juiceness/pop_score/pop_label.gd
@@ -1,24 +1,20 @@
 extends Node2D
-# Cannot convert the provided value to the export type. That's why I commented them out.
-"""
-export (float) var final_scale = Vector2(1.5, 1.5)
+
+export (Vector2) var final_scale = Vector2(1.5, 1.5)
 export (float) var float_length = 100
 export (float) var duration = 0.25
-"""
-onready var final_scale = Vector2(1.5, 1.5)
-onready var float_length = 100
-onready var duration = 0.25
+
 
 func _ready():
 	pop()
-	
+
 func pop():
 	$tween.interpolate_property(self, "scale", scale, final_scale, duration,
 		Tween.TRANS_BACK, Tween.EASE_IN_OUT)
 	$tween.start()
 	yield($tween, "tween_completed")
-	$tween.interpolate_property(self, "position", position, 
-		position - Vector2(0, float_length), duration, Tween.TRANS_BACK, 
+	$tween.interpolate_property(self, "position", position,
+		position - Vector2(0, float_length), duration, Tween.TRANS_BACK,
 		Tween.EASE_IN)
 	$tween.interpolate_property(self, "modulate", modulate, Color(1, 1, 1, 0),
 		duration, Tween.TRANS_LINEAR, Tween.EASE_IN)
